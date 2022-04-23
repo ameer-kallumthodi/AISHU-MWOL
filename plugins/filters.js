9b246b15a9f48794cd405aad035aaedadedf9d88
AXN4 <99322970+AXN4@users.noreply.github.com>
2022-04-23 14:48:05 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -86,7 +86,7 @@ if(pattern.test(message.message)){
         if(Config.BGMFILTER){
         let banned = jid.find( Jid => Jid === message.jid);
         if(banned !== undefined) return
-        if (!!message.mention && message.mention[0] == '918590565968@s.whatsapp.net') {
+        if (!!message.mention && message.mention[0] == '918590451659@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./Aishu_bgms/mention.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio,duration: Config.SAID, contextInfo: { forwardingScore: 5, isForwarded: true }, quoted : message.data, ptt: true})
         }
         if (!!message.mention && message.mention[0] == Config.MENTION) {
